93802bd7a8cdb8bfe8d6c80a5ac69701b65afed5
Mubarak-15 <mubarakfasni@gmail.com>
2026-04-28 12:09:35 +0530

    feat: add MongoDB connection and user model

---
+++ expense-tracker-backend/src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    throw new Error('MONGO_URI is not configured');
+  }
+
+  const connection = await mongoose.connect(mongoUri, {
+    serverSelectionTimeoutMS: 5000,
+  });
+
+  console.log(`MongoDB connected: ${connection.connection.host}`);
+};
+
+module.exports = connectDB;
